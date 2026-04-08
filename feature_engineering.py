62c554524acc019f2dca382f8733302bae30f6ea
shioner3 <shion.maebashi@gmail.com>
2026-04-08 22:22:51 +0900

    feature_engineering.py を更新

--- feature_engineering.py
+++ feature_engineering.py
@@ -10,7 +10,7 @@ PARQUET_FILE = "stock_data/prices.parquet"
 TRAIN_SAVE_PATH = "ml_dataset.parquet"
 PREDICT_SAVE_PATH = "ml_dataset_latest.parquet"
 
-HOLD_DAYS = 7
+HOLD_DAYS = 5
 MIN_COUNT = 3000
 Z_WINDOW = 20
 
